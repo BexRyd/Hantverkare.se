5f72c5dbfea1553b5ce0129d54531b65a45967c7
dkingbrandt <dkingbrandt@hotmail.com>
2022-09-08 18:53:14 +0200

    commit on heroku

--- server/server.js
+++ server/server.js
@@ -4,26 +4,31 @@ const dotenv = require('dotenv');
 const bodyParser = require("body-parser");
 const routerServices = require("./Api/serviceAPI")
 const routerSignUp = require("./Api/SignUpAPI")
-/*   const cors = require("cors")   */
+const cors = require("cors")
 const cookieParser = require("cookie-parser");
-const db = require("./database/db")
+// const db = require("./database/db")
+const mongoose = require("mongoose")
 
 
 dotenv.config({ path: './config.env' });
 const app = express();
-const port = 8080;
+
+
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dkingbrandt:gorilla1986@cluster0.9zx9wmx.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-//
 
- /* app.use(cors({
+
+app.use(cors({
     orgin: "http://127.0.0.1:3000",
     credentials: true
-}))  */
+}))
 
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -35,6 +40,8 @@ app.use(routerSignUp);
 app.use(routerServices);
 
 
-app.listen(8080, () => {
-    console.log(`listening on port ${port}`);
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server is listening to port ${port}`)
+});
