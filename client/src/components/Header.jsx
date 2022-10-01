59f1fbbcf6dcb9f797fa107bf54b837e1611c57c
dkingbrandt <dkingbrandt@hotmail.com>
2022-10-01 13:33:10 +0200

    change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import "../css/header.css"
 import '../css/Adds.css'
-import {get, post} from "./../utility/fetchHealper"
+import { get, post } from "./../utility/fetchHealper"
 import LoginError from './LoginError'
 import Recaptcha from './ReCAPTCHA'
 
@@ -24,71 +24,76 @@ function Header(props) {
   const [name, setName] = useState("");
   const [emailConfirm, setEmailConfirm] = useState(""); // ska vara en validering för email endast på frontend- Ska jämföras med email och emailconfirm innan det skickas till backend.
   const [passwordConfirm, setPasswordConfirm] = useState("");
-  
+
   const [autorized, setAutorized] = useState(false);
 
 
   // ***************************  VALIDATION STATES  *****************************************//
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [emailEpmty, setEmailEmpty] = useState(false)
-    const [passwordEmpty, setPasswordEmpty] = useState(false)
-    const [emailError, setEmailError] = useState('Ange e-post')
-    const [passwordError, setPasswordEroor] = useState('Ange password')
-    const [formIsValid, setFormIsValid] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailEpmty, setEmailEmpty] = useState(false)
+  const [passwordEmpty, setPasswordEmpty] = useState(false)
+  const [emailError, setEmailError] = useState('Ange e-post')
+  const [passwordError, setPasswordEroor] = useState('Ange password')
+  const [formIsValid, setFormIsValid] = useState(false)
 
 
-// ****************************  VALIDATION FUNKTIONER  **************************************//
+
+  // ****************************  VALIDATION FUNKTIONER  **************************************//
 
   //VALIDATION FUNKTIONER
 
+  const handleSubmit = event => {
+    event.preventDefault();
+  }
+
   useEffect(() => {
-        if(emailError || passwordError) {
-            setFormIsValid(false)
-        } else {
-            setFormIsValid(true)
-        }
-    }, [emailError, passwordError]);
-
-
-
-    const emailHandler =(e)=> {
-        setEmail(e.target.value)
-        const isValidEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        if (!isValidEmail.test(e.target.value)) {
-            setEmailError('E-post är inte korrekt!')
-            } else {
-            setEmailError("")
-            }
-        }
-    
-
-    const passwordHandler = (e) => {
-        setPassword(e.target.value)
-        if(e.target.value.length < 6 || e.target.value.length > 12) {
-            setPasswordEroor('Lösenordet måste vara 6-12 tecken långt')
-            if(e.target.value) {
-                setPasswordEroor('Lösenordet är inte korrekt!')
-            }
-        } else {
-            setPasswordEroor("")
-        }
+    if (emailError || passwordError) {
+      setFormIsValid(false)
+    } else {
+      setFormIsValid(true)
     }
+  }, [emailError, passwordError]);
 
 
-    const blurHandler =(e)=> {
-        switch (e.target.name) {
-            case 'email':
-                setEmailEmpty(true)
-                break
-            case 'password':
-                setPasswordEmpty(true)
-                break
-        }
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value)
+    const isValidEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!isValidEmail.test(e.target.value)) {
+      setEmailError('E-post är inte korrekt!')
+    } else {
+      setEmailError("")
     }
+  }
 
-//****************************************************************************//
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value)
+    if (e.target.value.length < 6 || e.target.value.length > 12) {
+      setPasswordEroor('Lösenordet måste vara 6-12 tecken långt')
+      if (e.target.value) {
+        setPasswordEroor('Lösenordet är inte korrekt!')
+      }
+    } else {
+      setPasswordEroor("")
+    }
+  }
+
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailEmpty(true)
+        break
+      case 'password':
+        setPasswordEmpty(true)
+        break
+    }
+  }
+
+  //****************************************************************************//
 
 
 
@@ -100,7 +105,7 @@ function Header(props) {
     state(current => !current); //toggle
   }
 
-// HEADER CONTAINER
+  // HEADER CONTAINER
 
   return (
     <div className="appContainer">
@@ -112,42 +117,42 @@ function Header(props) {
         <div className="menu_buttons_container">
           <NavLink to="/" className='menu_link'>Hem</NavLink>
           <NavLink to="/Adds" className='menu_link'>Annonser</NavLink>
-          {!props.login?(
-          <NavLink to="/MinSida" className='menu_link'>Minsida</NavLink>
+          {!props.login ? (
+            <NavLink to="/MinSida" className='menu_link'>Minsida</NavLink>
           )
-          :null
-            }
+            : null
+          }
           <div className='buttons'>
             {console.log(props.login)}
-              {props.login? (
-              
+            {props.login ? (
+
               (<button className='btn_nav_red' onClick={() => {
 
-                  handlePopUp(setLogin);
-                  
-                }}>Logga in</button>) )
+                handlePopUp(setLogin);
 
-                : ( <button className='btn_nav_red' onClick={() => {
+              }}>Logga in</button>))
+
+              : (<button className='btn_nav_red' onClick={() => {
                 props.setLogginPage("");
-                
-                get("/logout")
-              }}>Logga ut</button> ) }
 
-               {/* { props.authorized!==""?( <button className='btn_nav_red' onClick={() => {
+                get("/logout")
+              }}>Logga ut</button>)}
+
+            {/* { props.authorized!==""?( <button className='btn_nav_red' onClick={() => {
                 props.setLogginPage("");
                 setAutorized("");
                 get("/logout")
               }}>Logga ut</button> ): null} */}
 
 
-              <div className='blurr'
-                style={{
-                  opacity: login ? '1' : '0',
-                  visibility: login ? 'visible' : 'hidden',
-                  zIndex: login ? '2' : '-2',
-                }}
-              >
-                {login ?(
+            <div className='blurr'
+              style={{
+                opacity: login ? '1' : '0',
+                visibility: login ? 'visible' : 'hidden',
+                zIndex: login ? '2' : '-2',
+              }}
+            >
+              {login ? (
 
                 <div className='popup_form'>
                   <div>
@@ -158,85 +163,85 @@ function Header(props) {
                     >&times; </p>
                     <div className="popup_login_form">
                       <h2 className='popUp--title_form'>Logga in</h2>
-                      <Form>
 
-                        <form>
-                          {(emailEpmty && emailError) && <div style={{color: 'red', fontSize:'12px'}}>{emailError}</div>}
-                          <input className='form_login_input' onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name="email" type="email" placeholder="Ange e-post" />
-                          {(passwordEmpty && passwordError) && <div style={{color: 'red', fontSize:'12px'}}>{passwordError}</div>}
-                          <input className='form_login_input' onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name='password' type="password" placeholder="Ange password"/>
-                        </form>
 
-                        <Recaptcha className='recaptcha_container'/>
+                      <div>
+                        {(emailEpmty && emailError) && <div style={{ color: 'red', fontSize: '12px' }}>{emailError}</div>}
+                        <input className='form_login_input' onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name="email" type="email" placeholder="Ange e-post" />
+                        {(passwordEmpty && passwordError) && <div style={{ color: 'red', fontSize: '12px' }}>{passwordError}</div>}
+                        <input className='form_login_input' onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name='password' type="password" placeholder="Ange password" />
+                      </div>
 
-                        <button disabled={!formIsValid} className="setForm_submit" type="submit"  id="login_btn"
-                        
-                          onClick = {()=>{
-                            post("/login",{
-                                
-                                  
-                                  email:loginEmail,
-                                  password: loginPassword,
-                               
+                      <Recaptcha className='recaptcha_container' />
 
-  
-                        }).then((response) =>{ 
-                         
-                         // props.setUser(response.data)
-                         
-                          
-                         if (response.data) {
-                           props.setLogginPage(response.data)
-                       handlePopUp(setLogin);
-                        setLoginEmail("");
-                        setLoginPassword("");
-                    }
-                    else{
-                      handlePopUp(setErrorLogin);
-                    }
-                        
-                          
-                           })
-                        
+                      <button className="setForm_submit" id="login_btn"
+
+                        onClick={() => {
+                          post("/login", {
+
+
+                            email: loginEmail,
+                            password: loginPassword,
+
+
+
+                          }).then((response) => {
+
+                            // props.setUser(response.data)
+
+
+                            if (response.data) {
+                              props.setLogginPage(response.data)
+                              handlePopUp(setLogin);
+                              setLoginEmail("");
+                              setLoginPassword("");
+                            }
+                            else {
+                              handlePopUp(setErrorLogin);
+                            }
+
+
+                          })
+
                         }}
 
 
-                        >
-                          Logga in
-                        </button >
-             {errorLogin?(
-                      <LoginError 
-                      setLoginError={(btnUseState) => {
-                      setErrorLogin(btnUseState);
-          
-        }}
-                      />
-                   )
-                :null
-                }
-                      </Form>
-                      </div>
+                      >
+                        Logga in
+                      </button >
+                      {errorLogin ? (
+                        <LoginError
+                          setLoginError={(btnUseState) => {
+                            setErrorLogin(btnUseState);
+
+                          }}
+                        />
+                      )
+                        : null
+                      }
+
                     </div>
                   </div>
-                  ) : null}
-              </div>
+                </div>
+              ) : null}
+            </div>
 
 
-              <button className='btn_nav' onClick={() => {
-                handlePopUp(setRegistrera);
-              }} >Registrera</button>
+            <button className='btn_nav' onClick={() => {
+              handlePopUp(setRegistrera);
+            }} >Registrera</button>
 
 
-              <div className='blurr'
-                style={{
-                  opacity: registrera ? '1' : '0',
-                  visibility: registrera ? 'visible' : 'hidden',
-                  zIndex: registrera ? '2' : '-2',
+            <div className='blurr'
+              style={{
+                opacity: registrera ? '1' : '0',
+                visibility: registrera ? 'visible' : 'hidden',
+                zIndex: registrera ? '2' : '-2',
 
-                }}
-              > 
-              {registrera?(
-              <div className='popup_form'>
+              }}
+            >
+              {registrera ? (
+                <div className='popup_form'>
                   <div>
                     <p className="popUp--close_form" onClick={() => {
                       handlePopUp(setRegistrera);
@@ -247,54 +252,54 @@ function Header(props) {
                       <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label>Ange namn</Form.Label>
-                          <Form.Control type="text" placeholder="Ange namn" onChange={ (e) => setName(e.target.value)} />
+                          <Form.Control type="text" placeholder="Ange namn" onChange={(e) => setName(e.target.value)} />
                           <Form.Label>Ange E-post</Form.Label>
-                          <Form.Control type="email" placeholder="Ange e-post" onChange={ (e) => setEmail(e.target.value)} />
+                          <Form.Control type="email" placeholder="Ange e-post" onChange={(e) => setEmail(e.target.value)} />
                           <Form.Label>Upprepa E-post</Form.Label>
-                          <Form.Control type="email" placeholder="Upprepa e-post" onChange={ (e) => setEmailConfirm(e.target.value)} />
-                          
+                          <Form.Control type="email" placeholder="Upprepa e-post" onChange={(e) => setEmailConfirm(e.target.value)} />
+
                           {/* <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                           </Form.Text> */}
-                          </Form.Group>
-                          <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Lösenord</Form.Label>
-                            <Form.Control type="password" placeholder="Minst 8 tecken" onChange={(e) => setPassword(e.target.value)} />
-                          </Form.Group>
-                          <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Upprepa lösenordet</Form.Label>
-                            <Form.Control type="password" placeholder="Minst 8 tecken" onChange={(e) => setPasswordConfirm(e.target.value)} />
-                          </Form.Group>
-                          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            {/*  <Form.Check type="checkbox" label="Bekräfta" /> */}
-                          </Form.Group>
-                        </Form>
-                        <button variant="primary"
-                            onClick={() => {
-                              post("/signUp", {
-                                name: name,
-                                email: email,
-                                password: password,
-                                passwordConfirm: passwordConfirm
-                              }).then((response)=>{
-                                   if (response.data) {
-                       handlePopUp(setRegistrera);
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                          <Form.Label>Lösenord</Form.Label>
+                          <Form.Control type="password" placeholder="Minst 8 tecken" onChange={(e) => setPassword(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                          <Form.Label>Upprepa lösenordet</Form.Label>
+                          <Form.Control type="password" placeholder="Minst 8 tecken" onChange={(e) => setPasswordConfirm(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                          {/*  <Form.Check type="checkbox" label="Bekräfta" /> */}
+                        </Form.Group>
+                      </Form>
+                      <button variant="primary"
+                        onClick={() => {
+                          post("/signUp", {
+                            name: name,
+                            email: email,
+                            password: password,
+                            passwordConfirm: passwordConfirm
+                          }).then((response) => {
+                            if (response.data) {
+                              handlePopUp(setRegistrera);
                             }
-                              })
+                          })
 
 
-                             }}
-                          >
-                            Registrera dig
-                          </button>
-                      </div>
+                        }}
+                      >
+                        Registrera dig
+                      </button>
                     </div>
                   </div>
-                  ) : null}
-              </div>
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
--- client/src/components/ReCAPTCHA.jsx
+++ client/src/components/ReCAPTCHA.jsx
@@ -1,16 +1,16 @@
 import ReCAPTCHA from 'react-google-recaptcha'
 
 
-const Recaptcha= (props) => {
+const Recaptcha = (props) => {
 
 
   function onChange(value) {
-    console.log('Captcha value:', value);
+
 
     const button = document.getElementById("login_btn")
-    if(value === undefined){
-      button.disabled= true
-    }else{
+    if (value === undefined) {
+      button.disabled = true
+    } else {
       button.removeAttribute("disabled");
     }
   }
@@ -18,7 +18,7 @@ const Recaptcha= (props) => {
 
   return (
     <div className="App">
-      <ReCAPTCHA 
+      <ReCAPTCHA
         sitekey="6LdqMAwiAAAAADCSKAnd9VLheBJj3oFwBpicdF4N"
         onChange={onChange}
       />
